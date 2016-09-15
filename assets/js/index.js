3000c6174e557c79809b764503582556469e9d5a
Aaron D Borden <adborden@a14n.net>
2016-09-14 20:23:31 -0700

    GH Pages

---
+++ assets/js/index.js
@@ -0,0 +1,5 @@
+/* assets/js/index.js
+ *
+ * Add  your custom javascript here. This file is only loaded on the home page
+ * unless you add a `javascript` property to your page's front matter.
+ **/
